ee20c6202dbdb2f80016f67a48b92d8924be0f04
Raphael Nembhard <raphaelnembhard@Raphaels-MacBook-Pro.local>
2023-09-24 14:13:31 -0400

    Staking added

--- contracts/STE.sol
+++ contracts/STE.sol
@@ -10,10 +10,10 @@ contract StakeTOEarn is ERC20Pausable, Ownable{
 
     uint256 public salePrice = 0.01 ether;
     uint256 public totatlStaked = 0;
+    uint256 public userBalance = 0;
 
-    mapping(address => uint256) public staked;
-    mapping(address => uint256) public getBalance;
-
+    // Mapping of User who staked and the amount of tokens staked
+    mapping(address => mapping(address => UserStake)) public userStakes;
 
     constructor(uint256 _initialSupply) ERC20("Stack To Earn", "STE") {}
 
@@ -24,16 +24,71 @@ contract StakeTOEarn is ERC20Pausable, Ownable{
         _mint(_to, _amount);
     }
 
-    // User can stake tokens earn rewards
-    function stake(uint256 _amount) external {
-        require(_amount > 0, "Cannot stake 0 Tokens");
-        require(balanceOf(msg.sender) >= _amount, "Cannot stake more tokens than you hold unstaked");
+//   function createIncentive(
+//         address token,
+//         address rewardToken,
+//         uint112 rewardAmount,
+//         uint32 startTime,
+//         uint32 endTime
+//     ) external nonReentrant returns (uint256 incentiveId) {
 
-        staked[msg.sender] += _amount;
-        totatlStaked += _amount;
+//         if (rewardAmount <= 0) revert InvalidInput();
+
+//         if (startTime < block.timestamp) startTime = uint32(block.timestamp);
+
+//         if (startTime >= endTime) revert InvalidTimeFrame();
+
+//         unchecked { incentiveId = ++incentiveCount; }
+
+//         if (incentiveId > type(uint24).max) revert IncentiveOverflow();
+
+//         _saferTransferFrom(rewardToken, rewardAmount);
+
+//         incentives[incentiveId] = Incentive({
+//             creator: msg.sender,
+//             token: token,
+//             rewardToken: rewardToken,
+//             lastRewardTime: startTime,
+//             endTime: endTime,
+//             rewardRemaining: rewardAmount,
+//             liquidityStaked: 0,
+//             // Initial value of rewardPerLiquidity can be arbitrarily set to a non-zero value.
+//             rewardPerLiquidity: type(uint256).max / 2
+//         });
+
+//         emit IncentiveCreated(token, rewardToken, msg.sender, incentiveId, rewardAmount, startTime, endTime);
+
+//     }
+
+    function stakeToken(address token, uint256 amount) public nonReentrant {
+
+        _saferTransferFrom(token, amount);
+
+        UserStake storage userStake = userStakes[msg.sender][token];
+
+        uint112 previousLiquidity = userStake.liquidity;
+
+        userStake.liquidity += amount;
+
     }
 
-    
+    function unstakeToken(address token, uint112 amount, bool transferExistingRewards) external nonReentrant {
+
+        UserStake storage userStake = userStakes[msg.sender][token];
+
+        uint112 previousLiquidity = userStake.liquidity;
+
+        if (amount > previousLiquidity) revert InsufficientStakedAmount();
+
+        userStake.liquidity -= amount;
+
+        uint256 n = userStake.subscribedIncentiveIds.countStoredUint24Values();
+
+        ERC20(token).safeTransfer(msg.sender, amount);
+
+        emit Unstake(token, msg.sender, amount);
+
+    }
 
     function pause(bool state) external onlyOwner {
         if (state) {
