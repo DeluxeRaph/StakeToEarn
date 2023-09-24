d3c83cf40d6d81d2a015fe18abab8d5796e509a4
Raphael Nembhard <raphaelnembhard@Raphaels-MBP.fios-router.home>
2023-09-24 06:43:24 -0400

    Added Staking

--- contracts/STE.sol
+++ contracts/STE.sol
@@ -2,23 +2,38 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StackTOEarn is ERC20Pausable {
-    address public owner;
+contract StakeTOEarn is ERC20Pausable, Ownable{
 
-    modifier onlyOwner() {
-        require(msg.sender == owner); 
-        _;
-    }
+    using Address for address payable;
 
-    constructor(uint256 _initialSupply) ERC20("Stack To Earn", "STE") {
-        owner = msg.sender;
-        _mint(owner, _initialSupply);
-    }
+    uint256 public salePrice = 0.01 ether;
+    uint256 public totatlStaked = 0;
 
-    function mint(address _to, uint256 _amount) external onlyOwner {
+    mapping(address => uint256) public staked;
+    mapping(address => uint256) public getBalance;
+
+
+    constructor(uint256 _initialSupply) ERC20("Stack To Earn", "STE") {}
+
+    function buyTokens(uint256 _amount, address _to) external payable {
+        require(_to != address(0), "invalid to");
+        require(msg.value * 100 == _amount, "wrong amount of ETH sent");
+
         _mint(_to, _amount);
     }
+
+    // User can stake tokens earn rewards
+    function stake(uint256 _amount) external {
+        require(_amount > 0, "Cannot stake 0 Tokens");
+        require(balanceOf(msg.sender) >= _amount, "Cannot stake more tokens than you hold unstaked");
+
+        staked[msg.sender] += _amount;
+        totatlStaked += _amount;
+    }
+
+    
 
     function pause(bool state) external onlyOwner {
         if (state) {
@@ -26,5 +41,9 @@ contract StackTOEarn is ERC20Pausable {
         } else {
             Pausable._unpause();
         }
+    }
+
+    function withdraw() external onlyOwner {
+        payable(owner()).sendValue(address(this).balance);
     }
 }
